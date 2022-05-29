0588492b63a411606ceb5ece7cf492b0f4d3687b
mihir237 <mihir23701@gmail.com>
2022-05-29 12:45:39 +0530

    commit

--- note_app_test/settings.py
+++ note_app_test/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'nc*k8f@i+cvr6l(wkhfm+_2q9f^7qunhsuz8pz@(pdh@o34je('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','django-notes-app-2.herokuapp.com']
 
 
 # Application definition
